b6e8441c76b5adcb2e0003712826b559bbee72cd
bhrum <bhrumom@gmail.com>
2026-09-25 12:05:19 +0800

    feat(utils): expose implemented utility modules

--- source/packages/utils/src/lib.rs
+++ source/packages/utils/src/lib.rs
@@ -2,3 +2,10 @@ pub mod admin_command_denylist;
 pub mod attempt;
 pub mod canvas_path;
 pub mod command_glob;
+
+pub mod repo_url;
+pub mod oom_score_adj;
+pub mod workspace_paths;
+pub mod model_utils;
+pub mod web_search_year_guidance;
+pub mod ttl_cache;
